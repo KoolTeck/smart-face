14b86a5a1310ffc1c2f7c2cbe98a162f2897fcd2
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2020-09-08 18:46:19 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://pacific-basin-15764.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
